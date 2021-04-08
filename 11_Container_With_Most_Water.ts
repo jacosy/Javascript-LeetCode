9fb6098fe6f4c3aaea475fb4f3a5995207bc4f4a
Longoria Yu <longoria.you@gmail.com>
2021-04-09 00:00:05 +0800

    Solved 11_Container_With_Most_Water

---
+++ 11_Container_With_Most_Water.ts
@@ -0,0 +1,24 @@
+function maxArea(height: number[]): number {  
+  let left = 0;
+  let right = height.length - 1;
+  let max = (right - left) * Math.min(height[left], height[right]);
+  
+  while (left < right) {    
+    if (height[left] === height[right]) {      
+      max = Math.max(max, (right - left) * height[left]);
+      left += 1;
+      right -= 1;
+    } else if (height[left] > height[right]) {      
+      max = Math.max(max, (right - left) * height[right]);
+      right -= 1;
+    } else if (height[left] < height[right]) {
+      max = Math.max(max, (right - left) * height[left]);
+      left += 1;
+    }
+  }
+    
+  return max;
+};
+
+const height = [1,8,6,2,5,4,8,3,7];
+console.log(`Container With Most Water of height: [${height}] is`, maxArea(height));
