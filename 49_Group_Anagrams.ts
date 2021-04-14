69c4842df14151aed40d96165af725c44651e419
Longoria Yu <longoria.you@gmail.com>
2021-04-15 00:18:10 +0800

    Solved 49. Group Anagrams By Using Hashing Function

--- 49_Group_Anagrams.ts
+++ 49_Group_Anagrams.ts
@@ -12,5 +12,34 @@ function groupAnagrams(strs: string[]): string[][] {
   return [...map.values()];
 };
 
+function groupAnagramsByHashing(strs: string[]): string[][] {
+  const map = new Map<string, number>();
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+  for (let i=0; i < lowerCaseLetters.length; i++) {
+    map.set(lowerCaseLetters[i], 1 << i);
+  }
+  
+  const anagramMap = new Map<string, string[]>();
+  for (let s of strs) {
+    let sum = 0;
+    let charCodeSum = 0;
+    for (let i=0; i < s.length; i++) {
+      sum += map.get(s[i]) || 0;
+      charCodeSum += s.charCodeAt(i);
+    }
+    const sumKey = `${sum}${charCodeSum}${s.length}`;
+
+    if (anagramMap.has(sumKey)) {
+      const anagrams = anagramMap.get(sumKey);
+      anagrams?.push(s);
+    } else {
+      anagramMap.set(sumKey, [s]);
+    }
+  }
+  
+  return [...anagramMap.values()];
+};
+
 const strs = ["eat","tea","tan","ate","nat","bat","sao","hay","hay","rig","rig"];
 console.log(`Group Anagrams of "[${strs}]"`, groupAnagrams(strs));
+console.log(`Group Anagrams of "[${strs}]" By Using Hashing Function`, groupAnagramsByHashing(strs));
