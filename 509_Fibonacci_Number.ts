ef35117af1bacc9566dfe3501c573071755f5945
Longoria Yu <longoria.you@gmail.com>
2021-04-16 00:13:09 +0800

    Solved 509. Fibonacci Number by Using Buttom Up Approach

--- 509_Fibonacci_Number.ts
+++ 509_Fibonacci_Number.ts
@@ -17,7 +17,25 @@ function fib(n: number): number {
   }
 };
 
+function fib_buttomUp(n: number): number {
+  if (n < 2) {
+    return n;
+  }
+  
+  let pre = 0;
+  let sum = 1;
+  for (let i=2; i <= n; i++) {
+    sum += pre;
+    pre = sum - pre;
+  }
+  return sum;
+};
+
 const n = 10;
 console.log(`Fibonacci Number - '${n-2}' is`, fib(n-2));
 console.log(`Fibonacci Number - '${n-1}' is`, fib(n-1));
 console.log(`Fibonacci Number - '${n}' is`, fib(n));
+
+console.log(`Fibonacci Number by Bottom Up Approach - '${n-2}' is`, fib_buttomUp(n-2));
+console.log(`Fibonacci Number by Bottom Up Approach - '${n-1}' is`, fib_buttomUp(n-1));
+console.log(`Fibonacci Number by Bottom Up Approach - '${n}' is`, fib_buttomUp(n));
